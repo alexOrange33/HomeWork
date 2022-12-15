93e3714ac6ad093fe2c8494a0053189a78c62443
alex <gubin30101989@mail.ru>
2022-12-15 12:41:58 +0300

    Проверка на четность

--- HomeWork1/Program.cs
+++ HomeWork1/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine()!);
+
+if(num%2 == 0)
+{
+    Console.Write("Да");
+}
+else
+{
+    Console.Write("Нет");
+}
