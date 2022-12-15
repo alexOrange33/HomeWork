4c829f4f005dee892591d3b667d2a54a922ed5a6
alex <gubin30101989@mail.ru>
2022-12-15 12:04:08 +0300

    Большее из двух чисел

--- HomeWork1/Program.cs
+++ HomeWork1/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿Console.Write("Введите первое число: ");
+double num1 = Convert.ToDouble(Console.ReadLine()!);
+Console.Write("Введите второе число: ");
+double num2 = Convert.ToDouble(Console.ReadLine()!);
+
+if(num1 == num2)
+{
+    Console.Write($"{num1} = {num2}");
+}
+else
+{
+    if(num1>num2)
+    {
+        Console.Write($"{num1}>{num2}");
+    }
+    else
+    {
+        Console.Write($"{num2}>{num1}");
+    }
+}
