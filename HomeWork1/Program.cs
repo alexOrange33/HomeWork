c718eab4e68261dd1ecfcdd47cadfe9f18450565
alex <gubin30101989@mail.ru>
2022-12-15 12:28:04 +0300

    Большее из трех чисел

--- HomeWork1/Program.cs
+++ HomeWork1/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿Console.Clear();
+Console.Write("Введите первое число: ");
+double num1 = Convert.ToDouble(Console.ReadLine()!);
+Console.Write("Введите второе число: ");
+double num2 = Convert.ToDouble(Console.ReadLine()!);
+Console.Write("Введите третье число: ");
+double num3 = Convert.ToDouble(Console.ReadLine()!);
+
+double max = num1;
+if(max<num2)
+{
+    max = num2;
+}
+if(max<num3)
+{
+    max = num3;
+}
+Console.Write(max);
