4f2edfae66b03b05653bbc46ea62dfe04f7f037b
alex <gubin30101989@mail.ru>
2022-12-15 12:32:31 +0300

    Большее из трех чисел(поправка)

--- HomeWork1/Program.cs
+++ HomeWork1/Program.cs
@@ -1,20 +1,21 @@
-﻿Console.Write("Введите первое число: ");
+﻿Console.Clear();
+Console.Write("Введите первое число: ");
 double num1 = Convert.ToDouble(Console.ReadLine()!);
 Console.Write("Введите второе число: ");
 double num2 = Convert.ToDouble(Console.ReadLine()!);
 
 if(num1 == num2)
 {
-    Console.Write($"{num1} = {num2}");
+    Console.Write($"Числа равны: {num1} = {num2}");
 }
 else
 {
     if(num1>num2)
     {
-        Console.Write($"{num1}>{num2}");
+        Console.Write($"max = {num1}");
     }
     else
     {
-        Console.Write($"{num2}>{num1}");
+        Console.Write($"max = {num2}");
     }
 }
